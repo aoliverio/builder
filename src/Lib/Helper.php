57d43cb4264c95f897cf5bda50c284cc32255466
Antonio Oliverio <antonio.oliverio@gmail.com>
2016-03-06 16:11:40 +0100

    Create Lib directory

---
+++ src/Lib/Helper.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Builder\Lib;
+
+/**
+ * 
+ */
+class Helper {
+
+    /**
+     * 
+     * @return type
+     */
+    public static function getTemplates() {
+        return [
+            'checkbox' => '<input type="checkbox" name="{{name}}" value="{{value}}"{{attrs}}>',
+            'checkboxWrapper' => '<div class="checkbox">{{label}}</div>',
+            'input' => '<input class="form-control" type="{{type}}" name="{{name}}"{{attrs}}/>',
+            'textarea' => '<textarea class="form-control" name="{{name}}"{{attrs}}>{{value}}</textarea>',
+            'select' => '<select class="form-control" name="{{name}}"{{attrs}}>{{content}}</select>',
+            'selectMultiple' => '<select class="form-control" name="{{name}}[]" multiple="multiple"{{attrs}}>{{content}}</select>',
+        ];
+    }
+
+}
