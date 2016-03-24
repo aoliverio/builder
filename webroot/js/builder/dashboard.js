6b0e34491b8459d44a670a16cecae86caaadaf69
Antonio Oliverio <antonio.oliverio@gmail.com>
2016-03-24 16:47:28 +0100

    Update dashboard.js

--- webroot/js/builder/dashboard.js
+++ webroot/js/builder/dashboard.js
@@ -1,10 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Define layout scripts
  */
-$(document).ready(function () {
-    $('.dataTable').dataTable();
-});
-
-
