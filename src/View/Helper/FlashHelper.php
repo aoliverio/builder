97b9ab8559e167184d3caa15f28cd2fe2d3715d0
Antonio Oliverio <antonio.oliverio@gmail.com>
2016-03-26 14:26:36 +0100

    Adding FlashHelper in Builder/View/Helper

---
+++ src/View/Helper/FlashHelper.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Builder\View\Helper;
+
+use Cake\View\Helper;
+
+/**
+ * FlashHelper class to render flash messages.
+ *
+ */
+class FlashHelper extends Helper {
+
+    /**
+     * 
+     */
+    public function initialize() {
+        parent::initialize();
+    }
+
+}
