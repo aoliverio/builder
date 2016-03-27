43b0a6b82710fd988201b8990b34033b49824984
Antonio Oliverio <antonio.oliverio@gmail.com>
2016-03-27 14:27:30 +0200

    Added schema for an integrated simple RBAC system

---
+++ config/schema.sql
@@ -0,0 +1,89 @@
+-- -----------------------------------------------------
+-- Schema simple_rbac
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Table `users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NULL,
+  `username` VARCHAR(255) NULL,
+  `password` VARCHAR(255) NULL,
+  `is_blocked` TINYINT(1) NULL,
+  `created` TIMESTAMP NULL,
+  `modified` DATETIME NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `roles`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `description` TEXT NULL,
+  `created` TIMESTAMP NULL,
+  `modified` DATETIME NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tasks`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `routes` VARCHAR(255) NULL,
+  `description` TEXT NULL,
+  `created` TIMESTAMP NULL,
+  `modified` DATETIME NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `users_roles`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `users_roles` (
+  `users_id` INT NOT NULL,
+  `roles_id` INT NOT NULL,
+  PRIMARY KEY (`users_id`, `roles_id`),
+  INDEX `roles_key1_idx` (`roles_id` ASC),
+  INDEX `users_key_idx` (`users_id` ASC),
+  CONSTRAINT `users_key`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `roles_key1`
+    FOREIGN KEY (`roles_id`)
+    REFERENCES `roles` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `roles_tasks`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `roles_tasks` (
+  `roles_id` INT NOT NULL,
+  `tasks_id` INT NOT NULL,
+  PRIMARY KEY (`roles_id`, `tasks_id`),
+  INDEX `tasks_key1_idx` (`tasks_id` ASC),
+  INDEX `roles_key2_idx` (`roles_id` ASC),
+  CONSTRAINT `roles_key2`
+    FOREIGN KEY (`roles_id`)
+    REFERENCES `roles` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `tasks_key1`
+    FOREIGN KEY (`tasks_id`)
+    REFERENCES `tasks` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
