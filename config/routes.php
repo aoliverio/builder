f7200608dff816a2f271cebdfac4e1c79cc51c7e
Antonio Oliverio <antonio.oliverio@gmail.com>
2016-03-28 22:22:10 +0200

    Adding pages route

--- config/routes.php
+++ config/routes.php
@@ -3,4 +3,9 @@ use Cake\Routing\Router;
 
 Router::plugin('Builder', function ($routes) {
     $routes->fallbacks('InflectedRoute');
+    
+    /**
+     * 
+     */
+    $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
 });
