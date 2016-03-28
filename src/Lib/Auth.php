af1e484144d6dec94380f377df27f7f6bc59b2c4
Antonio Oliverio <antonio.oliverio@gmail.com>
2016-03-28 15:30:06 +0200

    Update Auth function in Builder/lib

--- src/Lib/Auth.php
+++ src/Lib/Auth.php
@@ -18,7 +18,56 @@ class Auth {
      * @return boolean
      */
     public static function isAuthorized($user_id, Request $request) {
-        return true;
+
+        $url_elements = explode('/', $request->url);
+        
+        /**
+         * Find roles by $user_id
+         */
+        $usersTables = TableRegistry::get('Builder.Users');
+        $query = $usersTables->find('all');
+        $query->where(['id' => $user_id]);
+        $query->contain(['Roles']);
+        $roles = $query->first()->roles;
+        
+        /**
+         * Define $userRolesId for passed $user_id
+         */
+        $userRolesId = [];
+        foreach($roles as $row)
+            $userRolesId[$row->id] = true;
+        
+        /**
+         * Find relative task in Builder.Tasks
+         */
+        $tasksTable = TableRegistry::get('Builder.Tasks');
+        do {
+            $route_target = '/' . implode('/', $url_elements);
+            $query = $tasksTable->find('all');
+            $query->where(['route' => $route_target]);
+            $query->contain(['Roles']); 
+            $result = $query->first();
+            
+            /**
+             * If user has roles authorized return true
+             */
+            if(count($result) > 0):
+                foreach($result->roles as $role):
+                    if(array_key_exists($role->id, $userRolesId))
+                        return true;
+                endforeach;
+            endif;
+              
+            /**
+             * Remove the element off the end of array
+             */
+            array_pop($url_elements);
+        } while ($route_target != '/');
+        
+        /**
+         * Defualt value
+         */
+        return false;
     }
 
 }
