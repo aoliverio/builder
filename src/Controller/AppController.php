183a5d87bf1e3fc8c0dc7c760aeac2896b09534e
Antonio Oliverio <antonio.oliverio@gmail.com>
2016-04-04 13:33:52 +0200

    Removed function version

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -8,12 +8,5 @@ use App\Controller\AppController as BaseController;
  * 
  */
 class AppController extends BaseController {
-
-    /**
-     * Print version
-     */
-    public function version() {
-        echo 'release 1.0.2';
-    }
-
+    
 }
