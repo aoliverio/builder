6c49f5b92a80b5738ce8b5113e87a0d258274eb7
Antonio Oliverio <antonio.oliverio@gmail.com>
2017-06-20 16:38:37 +0200

    Update PagesController.php

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -30,13 +30,13 @@ use Cake\View\Exception\MissingTemplateException;
 class PagesController extends AppController {
 
     /**
-     * Displays a view
+     * Displays a template
      *
      * @return void|\Cake\Network\Response
      * @throws \Cake\Network\Exception\NotFoundException When the view file could not
      *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
      */
-    public function view() {
+    public function display() {
         $path = func_get_args();
 
         $count = count($path);
